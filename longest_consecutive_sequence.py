b0047e9edfee6e4dc3de0f74dd38b0443ed81b78
josh <mckerracher@gmail.com>
2022-05-12 10:04:37 -0600

    More LC

---
+++ longest_consecutive_sequence.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def longestConsecutive(nums: List[int]):
+    max_length, numbers = 0, set(nums)
+
+    for i in range(len(nums)):
+        tmp_len = 1
+        if nums[i] - 1 not in numbers:
+            counter = nums[i] + 1
+            while counter in numbers:
+                tmp_len += 1
+                counter += 1
+        max_length = max(max_length, tmp_len)
+
+    return max_length
